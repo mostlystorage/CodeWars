29d18c3c438ad1deee5dd9ee81db3ca2016eda9b
mostlystorage <77912500L@gmail.com>
2024-07-29 20:12:02 +1000

    DigitalRoot Java problem

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(NumberUtils.isNarcissistic(55961));
-        System.out.println(NumberUtils.isNarcissistic(153));
+        System.out.println(NumberUtils.digitalRoot(1212));
+        System.out.println(NumberUtils.digitalRoot(872364));
     }
 }
--- src/NumberUtils.java
+++ src/NumberUtils.java
@@ -1,10 +1,21 @@
 public class NumberUtils {
     public static boolean isNarcissistic(int number) {
+        char[] digits = String.valueOf(number).toCharArray();
+        int sum = 0;
+        for (int i = 0; i < digits.length; i++) {
+            sum += (int) Math.pow((Integer.parseInt(String.valueOf(digits[i]))), digits.length);
+        }
+        return (sum == number);
+    }
+
+    public static int digitalRoot (int number) {
+        while (number > 9) {
             char[] digits = String.valueOf(number).toCharArray();
-            int sum = 0;
+            number = 0;
             for (int i = 0; i < digits.length; i++) {
-                sum += (int) Math.pow((Integer.parseInt(String.valueOf(digits[i]))), digits.length);
+                number += Integer.parseInt(String.valueOf(digits[i]));
             }
-            return (sum == number);
+        }
+        return number;
     }
 }
